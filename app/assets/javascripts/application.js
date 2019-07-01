530f85d93a6edda6bc9d8569a870798b31ddb817
smplyamzng <kharmon9452@gmail.com>
2019-07-01 13:39:21 -0400

    still need order fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require materialize
+//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
 
@@ -23,8 +24,51 @@
 	    var elems = document.querySelectorAll('.sidenav');
 	    var instances = M.Sidenav.init(elems, options);
 	    var options = {}
+
+	    
 	  });
 
 	 AOS.init();
 
- })
+	 
+
+	 // slider
+
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+	var slider_two = document.getElementById("myRange2");
+	var output_two = document.getElementById("demo2");	
+	var slider_three = document.getElementById("myRange3");
+	var output_three = document.getElementById("demo3");	
+	output.innerHTML = slider.value; // Display the default slider value
+	output_two.innerHTML = slider.value;
+	output_three.innerHTML = slider.value;
+	// Update the current slider value (each time you drag the slider handle)
+	slider.oninput = function() {
+	  output.innerHTML = this.value;
+	}
+
+	slider_two.oninput = function() {
+	  output_two.innerHTML = this.value;
+	}
+
+
+	slider_three.oninput = function() {
+	  output_three.innerHTML = this.value;
+	}
+
+	var special = document.getElementById("special_services_id")
+	var type = document.getElementById("service_type_id")
+
+	$(document).ready(function () {
+	  $('input[type=hidden]#special_services').val(special);
+	  $('input[type=hidden]#service_type').val(type);
+	});
+
+
+
+
+
+
+
+ });
